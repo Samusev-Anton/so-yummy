45dec12e72b152237f8a0781e7fcfd5d8b7d7dd3
Marry <masha432alex@i.ua>
2023-04-02 22:45:49 +0300

    add header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,19 @@
 // import styled from 'styled-components';
-import Header from "./Header/Header";
-import Hero from "./Hero/Hero";
-
 
 
 export const App = () => {
   return (
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101',
-    //   }}
-    // >
-    //   React homework template
-    // </div>
-    <>
-<Header/>
-<Hero/>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
   );
 };
