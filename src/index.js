110ee31899b2d8c504f44cf3eac7aa9a3266bd8e
Marry <masha432alex@i.ua>
2023-04-02 23:07:29 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './components/App';
 import './index.css';
 import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
-// import { store, persistor } from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter >
-        <App />
+      <BrowserRouter basename="/so-yummy">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
