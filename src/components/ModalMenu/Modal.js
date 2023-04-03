54da92f025c438a81509163e5e4b889920f915dc
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-03 06:34:07 +0300

    comments

--- src/components/ModalMenu/Modal.js
+++ src/components/ModalMenu/Modal.js
@@ -1,49 +1,43 @@
-import React, { useState } from 'react';
-import { createPortal } from "react-dom";
-import { useEffect } from "react";
-import { Overlay, ModalWindow } from "./Modal.styled";
+// import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
+import { Overlay, ModalWindow } from './Modal.styled';
 import CloseIcon from '@mui/icons-material/Close';
 import Logo from 'components/Logo/Logo';
 import { WraperHeaderModal } from 'components/Header/Header.styled';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import { styled } from '@mui/material/styles';
-
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Switch from '@mui/material/Switch';
+// import { styled } from '@mui/material/styles';
 
 const modalRoot = document.getElementById('modal-root');
 
-
 const Modal = ({ children, onClose }) => {
-
   useEffect(() => {
-
     document.addEventListener('keydown', closeModal);
-    document.body.style.overflow = "hidden";
-        return () => {
-          document.removeEventListener('keydown', closeModal);
-          document.body.style.overflow = ""; 
-        }
-   
-  })
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.removeEventListener('keydown', closeModal);
+      document.body.style.overflow = '';
+    };
+  });
 
   const closeModal = ({ target, currentTarget, code }) => {
-    if (target !== currentTarget || code === "Escape") {
+    if (target !== currentTarget || code === 'Escape') {
       onClose();
     }
-
-   
-  }
+  };
   return createPortal(
-    <Overlay onClick={closeModal} >
+    <Overlay onClick={closeModal}>
       <ModalWindow>
-    <WraperHeaderModal>
-    <Logo/>
-    <CloseIcon sx={{ fontSize: 32, color:'#22252A' }}/>
-    </WraperHeaderModal>
+        <WraperHeaderModal>
+          <Logo />
+          <CloseIcon sx={{ fontSize: 32, color: '#22252A' }} />
+        </WraperHeaderModal>
         {children}
-    </ModalWindow>
+      </ModalWindow>
     </Overlay>,
-    modalRoot);
-}
+    modalRoot
+  );
+};
 
 export default Modal;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,16 +3,16 @@ import {
   PrimaryNav,
   NavLink,
   NavMenu,
-  AuthMenu,
+  // AuthMenu,
   MobileContainer,
   TabletContainer,
   StyledBurger,
-} from "./Nav.styled";
+} from './Nav.styled';
 // import AuthNav from 'components/AuthNav/AuthNav';
 // import UserNav from 'components/UserNav/UserNav';
 // import { useAuth } from 'hooks';
 import { useMediaQuery } from 'react-responsive';
-import Modal from "components/ModalMenu/Modal";
+import Modal from 'components/ModalMenu/Modal';
 import SearchIcon from '@mui/icons-material/Search';
 import { UserMenu } from 'components/UserMenu/UserMenu';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -20,12 +20,11 @@ import Switch from '@mui/material/Switch';
 import { styled } from '@mui/material/styles';
 
 const Nav = () => {
-
   const isMobile = useMediaQuery({ maxWidth: 767 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
   const isDesktop = useMediaQuery({ minWidth: 1280 });
 
-//   const { isLoggedIn } = useAuth();
+  //   const { isLoggedIn } = useAuth();
 
   const [showModal, setShowModal] = useState(false);
   const MaterialUISwitch = styled(Switch)(({ theme }) => ({
@@ -41,12 +40,13 @@ const Nav = () => {
         transform: 'translateX(22px)',
         '& .MuiSwitch-thumb:before': {
           backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-            '#fff',
+            '#fff'
           )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
         },
         '& + .MuiSwitch-track': {
           opacity: 1,
-          backgroundColor: theme.palette.mode === 'white' ? '#8BAA36' : '#8BAA36',
+          backgroundColor:
+            theme.palette.mode === 'white' ? '#8BAA36' : '#8BAA36',
         },
       },
     },
@@ -64,7 +64,7 @@ const Nav = () => {
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          '#fff',
+          '#fff'
         )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
       },
     },
@@ -77,73 +77,87 @@ const Nav = () => {
 
   return (
     <>
-      {
-        isDesktop &&
-    <>
-        <PrimaryNav>
-        <NavMenu>
-          <NavLink to="/categories">Categories</NavLink>
-          <NavLink to="/add">Add recipes</NavLink>
-          <NavLink to="/my">My recipes</NavLink>
-          <NavLink to="/favorite">Favorites</NavLink>
-          <NavLink to="/shopping-list">Shopping list</NavLink>
-          <NavLink to="/search"> <SearchIcon sx={{ fontSize: 24 }}/></NavLink>
-        </NavMenu>
-          </PrimaryNav>
-  <UserMenu/>
-  <FormControlLabel
-        control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
-      />
-
-        </>
-      }
-      {isMobile &&
-      <>
-        <UserMenu/>
-        </>}
-      {isMobile && showModal && <Modal onClose={setShowModal}>
-        <MobileContainer>
+      {isDesktop && (
+        <>
           <PrimaryNav>
             <NavMenu>
-            <NavLink to="/categories">Categories</NavLink>
-          <NavLink to="/add">Add recipes</NavLink>
-          <NavLink to="/my">My recipes</NavLink>
-          <NavLink to="/favorite">Favorites</NavLink>
-          <NavLink to="/shopping-list">Shopping list</NavLink>
-          <NavLink to="search"><SearchIcon sx={{ fontSize: 20}}/>Search</NavLink>
+              <NavLink to="/categories">Categories</NavLink>
+              <NavLink to="/add">Add recipes</NavLink>
+              <NavLink to="/my">My recipes</NavLink>
+              <NavLink to="/favorite">Favorites</NavLink>
+              <NavLink to="/shopping-list">Shopping list</NavLink>
+              <NavLink to="/search">
+                {' '}
+                <SearchIcon sx={{ fontSize: 24 }} />
+              </NavLink>
             </NavMenu>
           </PrimaryNav>
-        </MobileContainer>
-      </Modal>}
+          <UserMenu />
+          <FormControlLabel
+            control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+          />
+        </>
+      )}
+      {isMobile && (
+        <>
+          <UserMenu />
+        </>
+      )}
+      {isMobile && showModal && (
+        <Modal onClose={setShowModal}>
+          <MobileContainer>
+            <PrimaryNav>
+              <NavMenu>
+                <NavLink to="/categories">Categories</NavLink>
+                <NavLink to="/add">Add recipes</NavLink>
+                <NavLink to="/my">My recipes</NavLink>
+                <NavLink to="/favorite">Favorites</NavLink>
+                <NavLink to="/shopping-list">Shopping list</NavLink>
+                <NavLink to="search">
+                  <SearchIcon sx={{ fontSize: 20 }} />
+                  Search
+                </NavLink>
+              </NavMenu>
+            </PrimaryNav>
+          </MobileContainer>
+        </Modal>
+      )}
 
-      {isTablet &&
-      <>
-        <UserMenu/>
-  <FormControlLabel control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
-      />
-      </>}
-      {isTablet && showModal && <Modal onClose={setShowModal}>
-        <TabletContainer>
-          <NavMenu>
-          <NavLink to="/categories">Categories</NavLink>
-          <NavLink to="/add">Add recipes</NavLink>
-          <NavLink to="/my">My recipes</NavLink>
-          <NavLink to="/favorite">Favorites</NavLink>
-          <NavLink to="/shopping-list">Shopping list</NavLink>
-          <NavLink to="search"></NavLink>
-          </NavMenu>
-        </TabletContainer>
-      </Modal>}
-      
-        <StyledBurger aria-label='menu' open={showModal} onClick={() => {
+      {isTablet && (
+        <>
+          <UserMenu />
+          <FormControlLabel
+            control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+          />
+        </>
+      )}
+      {isTablet && showModal && (
+        <Modal onClose={setShowModal}>
+          <TabletContainer>
+            <NavMenu>
+              <NavLink to="/categories">Categories</NavLink>
+              <NavLink to="/add">Add recipes</NavLink>
+              <NavLink to="/my">My recipes</NavLink>
+              <NavLink to="/favorite">Favorites</NavLink>
+              <NavLink to="/shopping-list">Shopping list</NavLink>
+              <NavLink to="search"></NavLink>
+            </NavMenu>
+          </TabletContainer>
+        </Modal>
+      )}
+
+      <StyledBurger
+        aria-label="menu"
+        open={showModal}
+        onClick={() => {
           setShowModal(current => !current);
           setShowModal(!showModal);
-        }}>
-          <div />
-          <div />
-          <div />
-        </StyledBurger>
-
+        }}
+      >
+        <div />
+        <div />
+        <div />
+      </StyledBurger>
     </>
   );
 };
