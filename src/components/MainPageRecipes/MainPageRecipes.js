f54329010c97c2ebdd09fdd2ee11ee751b57d8e4
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-06 21:17:22 +0300

    Update MainPageRecipes.js

--- src/components/MainPageRecipes/MainPageRecipes.js
+++ src/components/MainPageRecipes/MainPageRecipes.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
@@ -8,53 +7,55 @@ import { getMainCategories } from '../../redux/opertions';
 
 import { getContentForMain } from '../../redux/selectors';
 
-import { TitleCatigories, ListCatigories,ListMeals, ItemCategories, BtnRecipes, BtnRecipesLink } from './MainPageRecipes.styled';
+import {
+  TitleCatigories,
+  ListCatigories,
+  ListMeals,
+  ItemCategories,
+  BtnRecipesLink,
+} from './MainPageRecipes.styled';
 import { Container } from 'components/GlobalStyles';
 
-
-
 export const PreviewCategories = () => {
- const data = useSelector(getContentForMain);
- const dispatch = useDispatch();
- const isDesktop = useMediaQuery({ minWidth: 1440 });
- const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
-  
- useEffect(() => {
-  if (data !== null) return;
- dispatch(getMainCategories());
-}, [dispatch, data]);
-  
- let numCard;
- if (isDesktop) {
-numCard = 4; 
- } else if (isTablet) {
-numCard = 2; 
+  const data = useSelector(getContentForMain);
+  const dispatch = useDispatch();
+  const isDesktop = useMediaQuery({ minWidth: 1440 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
+
+  useEffect(() => {
+    if (data !== null) return;
+    dispatch(getMainCategories());
+  }, [dispatch, data]);
+
+  let numCard;
+  if (isDesktop) {
+    numCard = 4;
+  } else if (isTablet) {
+    numCard = 2;
   } else {
- numCard = 1; 
- };
+    numCard = 1;
+  }
   console.log(data);
-  if (!data) return
-   return (
+  if (!data) return;
+  return (
     <Container>
-  <ListCatigories>
- {data && data.map(meal => (
-  <ItemCategories key={meal._id}>
-  <TitleCatigories>{meal._id}</TitleCatigories>
- <ListMeals>
- {meal.firstFour?.slice(0, numCard).map(ingredient => (
-   
-<CardMeal key={ingredient._id} meal={ingredient} />
+      <ListCatigories>
+        {data &&
+          data.map(meal => (
+            <ItemCategories key={meal._id}>
+              <TitleCatigories>{meal._id}</TitleCatigories>
+              <ListMeals>
+                {meal.firstFour?.slice(0, numCard).map(ingredient => (
+                  <CardMeal key={ingredient._id} meal={ingredient} />
+                ))}
+              </ListMeals>
 
-  ))}
- </ListMeals>
-
- <BtnRecipesLink to={`/categories/${meal._id}`}>See all</BtnRecipesLink>
-
- </ItemCategories>
- 
-  ))}  
-  </ListCatigories>
-   </Container>
-);
-}
-
+              <BtnRecipesLink to={`/categories/${meal._id}`}>
+                See all
+              </BtnRecipesLink>
+            </ItemCategories>
+          ))}
+      </ListCatigories>
+    </Container>
+  );
+};
