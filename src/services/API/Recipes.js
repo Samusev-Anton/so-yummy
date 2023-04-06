3b2622b7946b0369943a29d8be33651316478386
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-06 16:46:32 +0300

    update search

--- src/services/API/Recipes.js
+++ src/services/API/Recipes.js
@@ -3,25 +3,26 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://so-yummy-backend.onrender.com/api';
 
 export const getCategoriesAPI = () => {
-    return axios.get('/recipes/categories').then(({ data }) => {
-        return data;
-    });
+  return axios.get('/recipes/categories').then(({ data }) => {
+    return data;
+  });
 };
 
-export const getRecipesByCategoryAPI = (category) => {
-    return axios.get(`/recipes/${category}`).then(({ data }) => {
-        return data;
-    });
-}
+export const getRecipesByCategoryAPI = category => {
+  return axios.get(`/recipes/${category}`).then(({ data }) => {
+    return data;
+  });
+};
 export const getSetOfCategoriestAPI = () => {
-    return axios.get('/recipes/main').then(({ data }) => {
-      return data;
-    });
-  };
+  return axios.get('/recipes/main').then(({ data }) => {
+    return data;
+  });
+};
 
 export const searchRecipesApi = async ({ searchQuery, searchType }) => {
-    const searchUrl = searchType === 'query' ? `/recipes/search/title?title=${searchQuery}` : `/ingredients/?ingredients=${searchQuery}`;
-    return axios.get(searchUrl).then(({ data }) => {
-        return data.data;
-    });
+  // const searchUrl = searchType === 'query' ? `/recipes/search/title?title=${searchQuery}` : `/ingredients/?ingredients=${searchQuery}`;
+
+  const searchUrl = `https://so-yummy-backend.onrender.com/api/recipes/search/title?title=${searchQuery}`;
+  const data = await axios.get(searchUrl);
+  return data.data.data;
 };
