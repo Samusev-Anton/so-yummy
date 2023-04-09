d6acb83ddfa5b3f643dff7a6a9642e137eb1d056
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-09 07:51:40 +0300

    Update RecipeIngredientsList.js

--- src/components/RecipeIngredientsList/RecipeIngredientsList.js
+++ src/components/RecipeIngredientsList/RecipeIngredientsList.js
@@ -2,31 +2,31 @@ import * as React from 'react';
 // import { useDispatch } from 'react-redux';
 // import { useSelector } from 'react-redux';
 // import { getRecipesIngredients } from '../../redux/selectors';
-import { Ingredient } from 'components/Ingredient/Ingredient';
-import { SectionTitle } from 'components/RecipePreparationFields/RecipePreparationFields.styled';
+// import { Ingredient } from 'components/Ingredient/Ingredient';
+// import { SectionTitle } from 'components/RecipePreparationFields/RecipePreparationFields.styled';
 
-export const RecipeIngredientsList = searchTerm => {
-  //   const dispatch = useDispatch();
-  // const ingerdients = useSelector(getRecipesIngredients);
+// export const RecipeIngredientsList = searchTerm => {
+//   //   const dispatch = useDispatch();
+//   // const ingerdients = useSelector(getRecipesIngredients);
 
-  //   const handleSubmit = event => {
-  //     event.preventDefault();
-  //   };
-  // const arr = [{}];
-  return (
-    <>
-      <SectionTitle>Ingredients</SectionTitle>
-      <ul>
-        <li>
-          <Ingredient searchTerm={searchTerm} />
-        </li>
-      </ul>
-      {/* <ul>
-        {ingerdients.map(el => (
-          <Ingredient key={el.id} {...el} />
-        ))}
-      </ul> */}
-    </>
+//   //   const handleSubmit = event => {
+//   //     event.preventDefault();
+//   //   };
+//   // const arr = [{}];
+//   return (
+//     <>
+//       <SectionTitle>Ingredients</SectionTitle>
+//       <ul>
+//         <li>
+//           <Ingredient searchTerm={searchTerm} />
+//         </li>
+//       </ul>
+//       {/* <ul>
+//         {ingerdients.map(el => (
+//           <Ingredient key={el.id} {...el} />
+//         ))}
+//       </ul> */}
+//     </>
 
 import {
   Table,
