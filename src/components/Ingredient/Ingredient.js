7b078c8a27233b34f3957e53e3bde9ee0fcb9c03
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-14 17:37:05 +0300

    fix and work addResipe

--- src/components/Ingredient/Ingredient.js
+++ src/components/Ingredient/Ingredient.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { useState } from 'react';
-// import { useEffect } from 'react';
 import Select from 'react-select';
 import IconButton from '@mui/material/IconButton';
 import {
@@ -10,47 +8,44 @@ import {
 import { stylesIngredient, stylesUnit } from '../AddRecipeForm/selectStyles';
 
 export const Ingredient = ({
-  id,
-  // title,
-  // weight,
-  // quantity,
-  // onTitleChange,
-  // onWeightChange,
-  // onQuantityChange,
+  ingredient,
   removeIngredientById,
   localTheme,
   theme,
   onSearchChange,
   titleOptions,
+  ingredients,
   onIngredientsChange,
 }) => {
-  const [recipeTitle, setRecipeTitle] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [width, setWidht] = useState('');
-  // const [newIng, setNewIng] = useState('');
-
   const handleTitleChange = e => {
-    setRecipeTitle(e.value);
+    const newArr = ingredients.map(item => {
+      if (item.id === ingredient.id) {
+        item.title = e.value;
+      }
+      return item;
+    });
+    onIngredientsChange(newArr);
   };
 
   const handleQuantityChange = e => {
-    // console.log(e.target.value);
-    setQuantity(e.target.value);
+    const newArr = [...ingredients].map(item => {
+      if (item.id === ingredient.id) {
+        item.quantity = e.target.value;
+      }
+      return item;
+    });
+    onIngredientsChange(newArr);
   };
 
   const handleWeightChange = e => {
-    setWidht(e.value);
+    const newArr = [...ingredients].map(item => {
+      if (item.id === ingredient.id) {
+        item.weight = e.value;
+      }
+      return item;
+    });
+    onIngredientsChange(newArr);
   };
-
-  const newIngredient = {
-    ttl: recipeTitle,
-    measure: `${quantity} ${width}`,
-  };
-
-  // setNewIng(newIngredient);
-  console.log(newIngredient);
-
-  // onIngredientsChange(); эта функция должна отправлять этот объект
 
   const handleSearchChange = e => {
     onSearchChange(e.target.value);
@@ -84,7 +79,6 @@ export const Ingredient = ({
         options={titleOptions}
         id="title"
         type="text"
-        value={recipeTitle}
         onChange={handleTitleChange}
         // onBlur={() => onTitleChange(id, title)}
         // onKeyDown={handleKeyDown}
@@ -94,7 +88,7 @@ export const Ingredient = ({
         <InputUnitValue
           id="quantity"
           type="number"
-          value={quantity}
+          value={ingredient.quantity}
           onChange={handleQuantityChange}
           // onBlur={() => onQuantityChange(id, quantity)}
           // onKeyDown={handleKeyDown}
@@ -104,14 +98,12 @@ export const Ingredient = ({
           styles={stylesUnit(theme, localTheme)}
           options={options}
           id="weight"
-          name="weight"
-          value={width}
           onChange={handleWeightChange}
           // onBlur={() => onWeightChange(id, weight)}
           // onKeyDown={handleKeyDown}
         ></Select>
       </ValueInputWrapper>
-      <IconButton onClick={() => removeIngredientById(id)}>
+      <IconButton onClick={() => removeIngredientById(ingredient.id)}>
         <svg
           width="18"
           height="19"
@@ -138,3 +130,141 @@ export const Ingredient = ({
     </>
   );
 };
+
+// import * as React from 'react';
+// import { useState } from 'react';
+// // import { useEffect } from 'react';
+// import Select from 'react-select';
+// import IconButton from '@mui/material/IconButton';
+// import {
+//   InputUnitValue,
+//   ValueInputWrapper,
+// } from '../../components/AddRecipeForm/AddRecipeForm.styled';
+// import { stylesIngredient, stylesUnit } from '../AddRecipeForm/selectStyles';
+
+// export const Ingredient = ({
+//   id,
+//   removeIngredientById,
+//   localTheme,
+//   theme,
+//   onSearchChange,
+//   titleOptions,
+//   onIngredientsChange,
+// }) => {
+//   const [recipeTitle, setRecipeTitle] = useState('');
+//   const [quantity, setQuantity] = useState('');
+//   const [width, setWidht] = useState('');
+//   // const [newIng, setNewIng] = useState('');
+//   console.log(titleOptions);
+
+//   const handleTitleChange = e => {
+//     setRecipeTitle(e.value);
+//     console.log(e.value);
+//   };
+
+//   const handleQuantityChange = e => {
+//     setQuantity(e.target.value);
+//   };
+
+//   const handleWeightChange = e => {
+//     setWidht(e.value);
+//     console.log(e.value);
+//   };
+
+//   const newIngredient = {
+//     ttl: recipeTitle,
+//     measure: `${quantity} ${width}`,
+//   };
+
+//   // setNewIng(newIngredient);
+//   console.log(newIngredient);
+
+//   // onIngredientsChange(newIngredient);
+//   // эта функция должна отправлять этот объект
+
+//   const handleSearchChange = e => {
+//     onSearchChange(e.target.value);
+//   };
+
+//   // const handleKeyDown = e => {
+//   //   if (e.key === 'Enter') {
+//   //     e.target.blur();
+//   //   }
+//   // };
+
+//   const options = [
+//     { value: 'tbs', label: 'tbs' },
+//     { value: 'tsp', label: 'tsp' },
+//     { value: 'kg', label: 'kg' },
+//     { value: 'g', label: 'g' },
+//   ];
+
+//   return (
+//     <>
+//       <InputUnitValue
+//         id="search"
+//         type="text"
+//         onChange={handleSearchChange}
+//         // onBlur={() => onQuantityChange(id, quantity)}
+//         // onKeyDown={handleKeyDown}
+//         placeholder="search"
+//       ></InputUnitValue>
+//       <Select
+//         styles={stylesIngredient(theme, localTheme)}
+//         options={titleOptions}
+//         id="title"
+//         type="text"
+//         value={recipeTitle}
+//         onChange={handleTitleChange}
+//         // onBlur={() => onTitleChange(id, title)}
+//         // onKeyDown={handleKeyDown}
+//         placeholder="Ingredient name"
+//       ></Select>
+//       <ValueInputWrapper>
+//         <InputUnitValue
+//           id="quantity"
+//           type="number"
+//           value={quantity}
+//           onChange={handleQuantityChange}
+//           // onBlur={() => onQuantityChange(id, quantity)}
+//           // onKeyDown={handleKeyDown}
+//           placeholder="Quantity"
+//         ></InputUnitValue>
+//         <Select
+//           styles={stylesUnit(theme, localTheme)}
+//           options={options}
+//           id="weight"
+//           name="weight"
+//           value={width}
+//           onChange={handleWeightChange}
+//           // onBlur={() => onWeightChange(id, weight)}
+//           // onKeyDown={handleKeyDown}
+//         ></Select>
+//       </ValueInputWrapper>
+//       <IconButton onClick={() => removeIngredientById(id)}>
+//         <svg
+//           width="18"
+//           height="19"
+//           viewBox="0 0 18 19"
+//           fill="none"
+//           xmlns="http://www.w3.org/2000/svg"
+//         >
+//           <path
+//             d="M14.0625 4.4375L3.9375 14.5625"
+//             stroke="#333333"
+//             strokeWidth="1.5"
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//           />
+//           <path
+//             d="M14.0625 14.5625L3.9375 4.4375"
+//             stroke="#333333"
+//             strokeWidth="1.5"
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//           />
+//         </svg>
+//       </IconButton>
+//     </>
+//   );
+// };
--- src/components/RecipeIngredientsList/RecipeIngredientsList.js
+++ src/components/RecipeIngredientsList/RecipeIngredientsList.js
@@ -20,45 +20,28 @@ import {
 } from '../AddRecipeForm/AddRecipeForm.styled';
 // import { Log } from 'styled-icons/octicons';
 
-export const RecipeIngredientsList = ({ onIngredientsChange }) => {
-  const [id, setId] = useState(nanoid());
-  const [ingredients, setIngredients] = useState([]);
+export const RecipeIngredientsList = ({ ingredients, onIngredientsChange }) => {
   const [allIngredientsAPI, setAllIngredientsAPI] = useState([]);
   const [options, setOptions] = useState([]);
 
   const addIngredientToArray = () => {
-    setId(nanoid());
-    const newObject = { id: id, title: '', quantity: '', weight: '' };
-    setIngredients([...ingredients, newObject]);
+    onIngredientsChange([
+      ...ingredients,
+      { id: nanoid(), title: '', quantity: '', weight: '' },
+    ]);
   };
-
-  // const handleAddIngredient = () => {
-  //   const data = JSON.stringify(
-  //     ingredients.map(({ id, quantity, title, weight }) => ({
-  //       id,
-  //       quantity,
-  //       title,
-  //       weight,
-  //     }))
-  //   );
-
-  //   console.log(data);
-
-  //   onIngredientsChange(data);
-  // };
 
   const removeIngredientFromArray = () => {
     const newArray = [...ingredients.slice(0, -1)];
-    setIngredients(newArray);
+    onIngredientsChange(newArray);
   };
 
   const removeIngredientById = id => {
-    const newArray = ingredients.filter(el => el.id !== id);
-    setIngredients(newArray);
+    const newArray = [...ingredients].filter(el => el.id !== id);
+    onIngredientsChange(newArray);
   };
 
   const onSearchChange = async text => {
-    console.log(text);
     try {
       if (text === '') {
         return;
@@ -76,25 +59,6 @@ export const RecipeIngredientsList = ({ onIngredientsChange }) => {
     } catch (error) {}
   };
 
-  const handleTitleChange = async (id, title) => {
-    // let array = [];
-    // setIngredients(allIngredientsAPI.find(item => item.ttl === title));
-    setIngredients(
-      ingredients.map(el => (el.id === id ? { ...el, title } : el))
-    );
-  };
-
-  const handleQuantityChange = (id, quantity) => {
-    setIngredients(
-      ingredients.map(el => (el.id === id ? { ...el, quantity } : el))
-    );
-  };
-
-  const handleWeightChange = (id, weight) => {
-    setIngredients(
-      ingredients.map(el => (el.id === id ? { ...el, weight } : el))
-    );
-  };
   return (
     <IngredientsSection
     // onKeyDown={handleAddIngredient}
@@ -104,26 +68,24 @@ export const RecipeIngredientsList = ({ onIngredientsChange }) => {
       <TitleConterWrapper>
         <IngredientsSectionTitle>Ingredients</IngredientsSectionTitle>
         <CounterWrapper>
-          <CounterButton onClick={removeIngredientFromArray}>-</CounterButton>
+          <CounterButton type="button" onClick={removeIngredientFromArray}>
+            -
+          </CounterButton>
           <span>{ingredients.length}</span>
-          <CounterButton onClick={addIngredientToArray}>+</CounterButton>
+          <CounterButton type="button" onClick={addIngredientToArray}>
+            +
+          </CounterButton>
         </CounterWrapper>
       </TitleConterWrapper>
       <IngredientsList>
         {ingredients.map(el => (
           <IngredientsItem key={el.id}>
             <Ingredient
-              {...el}
-              id={el.id}
-              title={el.title}
-              weight={el.weight}
-              quantity={el.quantity}
               titleOptions={options}
-              onTitleChange={handleTitleChange}
-              onQuantityChange={handleQuantityChange}
-              onWeightChange={handleWeightChange}
+              ingredient={el}
               removeIngredientById={removeIngredientById}
               onSearchChange={onSearchChange}
+              ingredients={ingredients}
               onIngredientsChange={onIngredientsChange}
             />
           </IngredientsItem>
@@ -132,3 +94,124 @@ export const RecipeIngredientsList = ({ onIngredientsChange }) => {
     </IngredientsSection>
   );
 };
+
+// import * as React from 'react';
+// import { useState } from 'react';
+// import { nanoid } from 'nanoid';
+// import { Ingredient } from 'components/Ingredient/Ingredient';
+// // import { SectionTitle } from 'components/AddRecipeForm/AddRecipeForm.styled';
+
+// import {
+//   IngredientsSection,
+//   CounterButton,
+//   CounterWrapper,
+//   TitleConterWrapper,
+// } from './RecipeIngredientsList.styled';
+
+// import {
+//   IngredientsList,
+//   IngredientsItem,
+//   IngredientsSectionTitle,
+//   // PopularTitle,
+// } from '../AddRecipeForm/AddRecipeForm.styled';
+// // import { Log } from 'styled-icons/octicons';
+
+// export const RecipeIngredientsList = ({
+//   onIngredientsChange,
+//   titleOptions,
+//   onSearchChange,
+// }) => {
+//   const [id, setId] = useState(nanoid());
+//   const [ingredients, setIngredients] = useState([]);
+
+//   const addIngredientToArray = () => {
+//     setId(nanoid());
+//     const newObject = { id: id, title: '', quantity: '', weight: '' };
+//     setIngredients([...ingredients, newObject]);
+//   };
+
+//   // const handleAddIngredient = () => {
+//   //   const data = JSON.stringify(
+//   //     ingredients.map(({ id, quantity, title, weight }) => ({
+//   //       id,
+//   //       quantity,
+//   //       title,
+//   //       weight,
+//   //     }))
+//   //   );
+
+//   //   console.log(data);
+
+//   //   onIngredientsChange(data);
+//   // };
+
+//   const removeIngredientFromArray = () => {
+//     const newArray = [...ingredients.slice(0, -1)];
+//     setIngredients(newArray);
+//   };
+
+//   const removeIngredientById = id => {
+//     const newArray = ingredients.filter(el => el.id !== id);
+//     setIngredients(newArray);
+//   };
+
+//   const handleTitleChange = async (id, title) => {
+//     // let array = [];
+//     // setIngredients(allIngredientsAPI.find(item => item.ttl === title));
+//     setIngredients(
+//       ingredients.map(el => (el.id === id ? { ...el, title } : el))
+//     );
+//   };
+
+//   const handleQuantityChange = (id, quantity) => {
+//     setIngredients(
+//       ingredients.map(el => (el.id === id ? { ...el, quantity } : el))
+//     );
+//   };
+
+//   const handleWeightChange = (id, weight) => {
+//     setIngredients(
+//       ingredients.map(el => (el.id === id ? { ...el, weight } : el))
+//     );
+//   };
+//   return (
+//     <IngredientsSection
+//     // onKeyDown={handleAddIngredient}
+//     // onMouseMove={handleAddIngredient}
+//     // onMouseLeave={handleAddIngredient}
+//     >
+//       <TitleConterWrapper>
+//         <IngredientsSectionTitle>Ingredients</IngredientsSectionTitle>
+//         <CounterWrapper>
+//           <CounterButton type="button" onClick={removeIngredientFromArray}>
+//             -
+//           </CounterButton>
+//           <span>{ingredients.length}</span>
+//           <CounterButton type="button" onClick={addIngredientToArray}>
+//             +
+//           </CounterButton>
+//         </CounterWrapper>
+//       </TitleConterWrapper>
+//       <IngredientsList>
+//         {ingredients.map(el => (
+//           <IngredientsItem key={el.id}>
+//             <Ingredient
+//               {...el}
+//               id={el.id}
+//               title={el.title}
+//               weight={el.weight}
+//               quantity={el.quantity}
+//               titleOptions={titleOptions}
+//               onTitleChange={handleTitleChange}
+//               onQuantityChange={handleQuantityChange}
+//               onWeightChange={handleWeightChange}
+//               removeIngredientById={removeIngredientById}
+//               onSearchChange={onSearchChange}
+//               onIngredientsChange={onIngredientsChange}
+//             />
+//           </IngredientsItem>
+//         ))}
+//       </IngredientsList>
+//     </IngredientsSection>
+//   );
+// };
